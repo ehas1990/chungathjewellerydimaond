3a7149089e4df926b4db243679f8c752a791d945
ehas1990 <ehas1990>
2022-10-21 15:18:40 +0530

    updation21-10-22

--- chugathu/mail_handler.php
+++ chugathu/mail_handler.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ERROR | E_PARSE);
 ini_set( 'display_errors', 1 );
 error_reporting( E_ALL );
 error_reporting(E_ERROR | E_PARSE);
@@ -8,17 +9,20 @@ $email= $_POST['email'];
 $sub= $_POST['sub'];
 $message= $_POST['message'];
 $subject="Royal chest Chungath Jewellery Diamond Boutique";
- $to="royal_chest@hotmail.com" ;
+$to="royal_chest@hotmail.com" ;
+//  $to="ehasalpha@gmail.com" ;
 
 $txt ="Name : ". $name . "\r\n phone : " . $phone . "\r\n  sub :" . $sub . "\r\n Message : ". $message;
 $sub= ". $sub";
 $headers = "From:" . $email;
- 
+ $status = null;
 if(mail($to,$subject,$txt,$headers)) {
  
-echo "<h1>Sent Successfully! Thank you ".$name."</h1>";
-} else {
-    echo "The email message was not sent.";
-}
+$status=1;
 
+} else {
+    $status=0;
+}
+header("Location:index.php?s=".$status);
+exit;
 ?>
